64060d1ad695fb10432414f501e932d5633ec4f2
Lars Noschinski <lars@public.noschinski.de>
2015-03-16 12:11:54 +0100

    rename to rewrite

--- Rewrite_Walkthrough.thy
+++ Rewrite_Walkthrough.thy
@@ -1,5 +1,5 @@
-theory Rewrite1_Walkthrough
-imports Complex_Main Rewrite1 "~~/src/HOL/Library/While_Combinator"
+theory Rewrite_Walkthrough
+imports Complex_Main Rewrite "~~/src/HOL/Library/While_Combinator"
 begin
 
 (* This file is intended to give an overview over
@@ -14,20 +14,20 @@ lemma
   shows "f ((a - a) + (a - a)) + f ((a - a) + c) = f 0 +  f c"
 
   (* First, from right to left, reduce the (a - a) terms to zero. *)
-  apply(rewrite1 in "f _ + f \<box> = _" diff_self)
-  apply(rewrite1 at "f (_ + \<box>) + f _ = _" diff_self)
-  apply(rewrite1 in "f \<box> + _ = _" diff_self)
+  apply(rewrite in "f _ + f \<box> = _" diff_self)
+  apply(rewrite at "f (_ + \<box>) + f _ = _" diff_self)
+  apply(rewrite in "f \<box> + _ = _" diff_self)
 
   (* Collapse zeros from left to right. *)
-  apply(rewrite1 at "0 + 0" add.left_neutral)
-  apply(rewrite1 at "0 + c" add.left_neutral)
+  apply(rewrite at "0 + 0" add.left_neutral)
+  apply(rewrite at "0 + c" add.left_neutral)
 
   apply(rule refl)
 done
 
 lemma
   fixes x y :: nat shows"x + y > c \<Longrightarrow> y + x > c"
-apply (rewrite1 at "\<box> > c" add.commute)
+apply (rewrite at "\<box> > c" add.commute)
 apply assumption
 done
 
@@ -35,21 +35,21 @@ done
 lemma 
 fixes x::nat and y::nat
 shows "x + y > c \<Longrightarrow> y + x > c"
-apply(rewrite1 in asm add.commute)
-apply(rewrite1 in "y + x > c" at asm add.commute)
-apply(rewrite1 at "\<box> > c" at asm add.commute)
+apply(rewrite in asm add.commute)
+apply(rewrite in "y + x > c" at asm add.commute)
+apply(rewrite at "\<box> > c" at asm add.commute)
 apply(assumption)
 done
 
 (* Pattern based rewriting on subterms containing bound variables. *)
 lemma "P {x::rat. y + 1 = x + 1}"
 (* The rightmost pattern binds the identifier x that can then later be reused. *)
-apply(rewrite1 at "x+1" in "{x::rat. \<box> }" add.commute)
+apply(rewrite at "x+1" in "{x::rat. \<box> }" add.commute)
 oops
 
 (* Slightly more complicated pattern. *)
 lemma "P {(x::nat, y::nat, z). x + z * 3 = Q (\<lambda>s t. y + s * t - 3)}"
-apply(rewrite1 at "b + d * e" in "\<lambda>(a, b, c). _ = Q (\<lambda>d e. \<box>)" add.commute)
+apply(rewrite at "b + d * e" in "\<lambda>(a, b, c). _ = Q (\<lambda>d e. \<box>)" add.commute)
 oops
 
 (* Rewriting with conditional rewriting rules works just as well. *)
@@ -62,9 +62,9 @@ lemma
 fixes f :: "nat \<Rightarrow> nat"
 shows "f x = y"
 (* If we apply the rule directly, the result's premises will contain a free schematic variable ?y. *)
-(*apply(rewrite1 test_theorem)*)
+(*apply(rewrite test_theorem)*)
 (* It makes sense to instantiate ?y beforehand. *)
-apply(rewrite1 at "f x" test_theorem where y = 0)
+apply(rewrite at "f x" test_theorem where y = 0)
 oops
 
 
@@ -72,7 +72,7 @@ oops
 lemma
 fixes f :: "nat \<Rightarrow> nat"
 shows "f x = P (\<lambda>(x::nat). 3 + x + y)"
-apply(rewrite1 at "3 + _" test_theorem where y=42)
+apply(rewrite at "3 + _" test_theorem where y=42)
 oops
 
 
@@ -92,31 +92,31 @@ definition "f_inv (I :: nat \<Rightarrow> bool) n \<equiv> f n"
    want to add an invariant to f. *)
 lemma "P (\<lambda>n. f n + 1) = x"
 (* Substitute f_inv for f and instantiate ?I with a simple invariant. *)
-apply(rewrite1 f_inv_def[symmetric] where I = "\<lambda>x. True")
-apply(rewrite1 f_inv_def)
+apply(rewrite f_inv_def[symmetric] where I = "\<lambda>x. True")
+apply(rewrite f_inv_def)
 
 (* We can also add an invariant that contains the variable n bound in the outer context.
    For this, we need to bind this variable to an identifier. *)
-apply(rewrite1 in "\<lambda>n. \<box>" f_inv_def[symmetric] where I = "\<lambda>x. n < x + 1")
-apply(rewrite1 f_inv_def)
+apply(rewrite in "\<lambda>n. \<box>" f_inv_def[symmetric] where I = "\<lambda>x. n < x + 1")
+apply(rewrite f_inv_def)
 
 (* Any identifier will work *)
-apply(rewrite1 in "\<lambda>abc. \<box>" f_inv_def[symmetric] where I = "\<lambda>x. abc < x + 1")
-apply(rewrite1 f_inv_def)
+apply(rewrite in "\<lambda>abc. \<box>" f_inv_def[symmetric] where I = "\<lambda>x. abc < x + 1")
+apply(rewrite f_inv_def)
 oops
 
 (* The "all" keyword. *)
 lemma "\<And>x y z. x + y + z = z + y + (x::int)"
-apply(rewrite1 at "x + y" in "x + y + z" in concl all (x y z) add.commute)
-apply(rewrite1 at "(y + _) + z" in concl all (y z) add.commute)
-apply(rewrite1 at "_" in concl all () add.commute)
+apply(rewrite at "x + y" in "x + y + z" in concl all (x y z) add.commute)
+apply(rewrite at "(y + _) + z" in concl all (y z) add.commute)
+apply(rewrite at "_" in concl all () add.commute)
 apply(simp)
 done
 
 (* It can be used anywhere in the pattern where there is an \<And>-Quantifier.
    TODO: This is still a little awkward. *)
-lemma "(\<And>(x::int). x + 1 > x) \<Longrightarrow> (x::int) + 1 > x"
-apply(rewrite1 at "x + 1" in goal all (x) at asm add.commute)
+lemma "(\<And>a (b::int). b + 1 > b) \<Longrightarrow> (x::int) + 1 > x"
+apply(rewrite at "x + 1" in goal all (y x) at asm add.commute)
 apply(simp)
 done
 
@@ -135,7 +135,7 @@ lemma "
       (0,0)
   ) = 12"
 (* We use pattern to specify exactly which while loop to annotate and also to give names to bound variables in the goal. *)
-apply(rewrite1 in "snd (while _ (\<lambda>(i, _). \<box>) _)" while_inv_def[symmetric] where ?I = "\<lambda>(j::nat, x). x = j + 3*i" )
+apply(rewrite in "snd (while _ (\<lambda>(i, _). \<box>) _)" while_inv_def[symmetric] where ?I = "\<lambda>(j::nat, x). x = j + 3*i" )
 oops
 
 end
